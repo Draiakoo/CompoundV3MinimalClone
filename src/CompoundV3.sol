6c620a601475400802a721c8d7841096a04338eb
Draiakoo <polureher@gmail.com>
2024-02-06 16:43:41 +0100

    Added supply, repay, withdraw and borrow functions

--- src/CompoundV3.sol
+++ src/CompoundV3.sol
@@ -7,13 +7,34 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 struct AssetInfo {
     address priceFeed;
-    uint256 loanToValue;
+    uint8 offset;
+    uint256 borrowCollateralFactor;
+    uint256 totalDeposited;
 }
 
 struct UserInfo {
-    int104 principal;
-    uint64 trackingIndex;
+    int256 principal; // positive for lenders, negative for borrowers
+    uint256 trackingIndex;
     uint16 assetsIn;
+}
+
+interface IPriceFeed {
+  function decimals() external view returns (uint8);
+
+  function description() external view returns (string memory);
+
+  function version() external view returns (uint256);
+
+  function latestRoundData()
+    external
+    view
+    returns (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    );
 }
 
 contract CompoundV3 {
@@ -33,11 +54,17 @@ contract CompoundV3 {
     uint256 public immutable borrowInterestSlopeLow;
     uint256 public immutable borrowInterestSlopeHigh;
 
+    uint256 public immutable baseBorrowMin;
+    address public immutable baseTokenPriceFeed;
+
     mapping(address collateralToken => AssetInfo tokenInfo) public tokenInfos;
+    mapping(uint16 index => address collateralToken) public indexToToken;
 
     mapping(address user => UserInfo info) public userInfos;
 
     mapping(address user => mapping(address token => uint256 collateralAmount)) public collaterals;
+
+
 
     uint256 public supplyInterestRate;
     uint256 public borrowInterestRate;
@@ -46,36 +73,159 @@ contract CompoundV3 {
     uint256 public lastAccrualTime;
 
     constructor() {
-        lastAcctualTime = block.timestamp;
     }
 
 
-    function supplyBase(uint256 amount) external {
+    function supplyOrRepayBase(uint256 _amount) external {
         // Transfer the tokens into this contract from the user
-        IERC20(baseToken).safeTransferFrom(msg.sender, address(this), amount);
+        IERC20(baseToken).safeTransferFrom(msg.sender, address(this), _amount);
 
         // Accrue indices
-        accureInternal();
+        accrueInternal();
         
         UserInfo memory userInfo = userInfos[msg.sender];
-        int104 principal = userInfo.principal;
+        int256 principal = userInfo.principal;
         // Unsafe cast to int256
-        int256 balance = presentValue(supplyInterestRate, principal) + int256(amount);
+        int256 balance = presentValue(principal) + int256(_amount);
+        int256 newPrincipal = principalValue(balance);
+
+        (uint256 repaidAmount, uint256 suppliedAmount) = repayAndSupplyAmount(principal, newPrincipal);
+
+        totalSupplyBase += suppliedAmount;
+        totalBorrowBase -= repaidAmount;
+
+        userInfo.principal = newPrincipal;
     }
 
-    function supplyCollateral(uint256 amount) external {}
+    // Notice there is no supplyCap for simplicity
+    function supplyCollateral(address _token, uint256 _amount) external {
+        AssetInfo memory assetInfo = tokenInfos[_token];
 
-    function borrowBase(uint256 amount) external {}
+        if(assetInfo.priceFeed == address(0)) revert();
 
-    function repayBase(uint256 amount) external {}
+        IERC20(_token).safeTransferFrom(msg.sender, address(this), _amount);
 
-    function withdrawBase(uint256 amount) external {}
+        assetInfo.totalDeposited += _amount;
+        uint256 userCollateral = collaterals[msg.sender][_token];
+        uint256 newUserCollateral = userCollateral + _amount;
 
-    function withdrawCollateral(uint256 amount) external {}
+        tokenInfos[_token] = assetInfo;
+
+        // Update assetsIn
+        if(userCollateral == 0 && newUserCollateral != 0){
+            userInfos[msg.sender].assetsIn |= uint16(1 << assetInfo.offset);
+        } else if(userCollateral != 0 && newUserCollateral == 0){
+            userInfos[msg.sender].assetsIn &= ~uint16(1 << assetInfo.offset);
+        }
+    }
+
+    function borrowOrWithdrawBase(uint256 _amount) external {
+        accrueInternal();
+
+        UserInfo memory userInfo = userInfos[msg.sender];
+        int256 principal = userInfo.principal;
+        int256 balance = presentValue(principal) - int256(_amount);
+        int256 newPrincipal = principalValue(balance);
+
+        (uint256 withdrawnAmount, uint256 borrowedAmount) = withdrawAndBorrowAmount(principal, newPrincipal);
+
+        totalSupplyBase -= withdrawnAmount;
+        totalBorrowBase += borrowedAmount;
+
+        userInfo.principal = newPrincipal;
+
+        if(balance < 0){
+            if(uint256(-balance) < baseBorrowMin) revert();
+            if(!isBorrowCollateralized(msg.sender)) revert();
+        }
+
+        IERC20(baseToken).safeTransfer(msg.sender, _amount);
+    }
+
+    function withdrawCollateral(address _token, uint256 _amount) external {
+        AssetInfo memory assetInfo = tokenInfos[_token];
+
+        if(assetInfo.priceFeed == address(0)) revert();
+
+        uint256 collateral = collaterals[msg.sender][_token];
+        uint256 newCollateral = collateral - _amount;
+
+        tokenInfos[_token].totalDeposited -= _amount;
+        collaterals[msg.sender][_token] = newCollateral;
+
+        // Update assetsIn
+        if(collateral == 0 && newCollateral != 0){
+            userInfos[msg.sender].assetsIn |= uint16(1 << assetInfo.offset);
+        } else if(collateral != 0 && newCollateral == 0){
+            userInfos[msg.sender].assetsIn &= ~uint16(1 << assetInfo.offset);
+        }
+
+        if(!isBorrowCollateralized(msg.sender)) revert();
+
+        IERC20(_token).safeTransfer(msg.sender, _amount);
+    }
+
+    function absorb(address[] calldata _accounts) external {}
+
+    function buyCollateral(address _token, uint256 minAmount, uint256 baseAmount) external {}
+
+    function transfer(address _receiver, uint256 _amount) external returns(bool) {}
 
 
 
 
+
+
+    function isBorrowCollateralized(address _user) internal view returns(bool){
+        int256 principal = userInfos[_user].principal;
+
+        if(principal >= 0) {
+            return true;
+        }
+
+        uint16 assetsIn = userInfos[_user].assetsIn;
+        int256 borrowAmountToFulfill = presentValue(principal) * int256(getPrice(baseTokenPriceFeed)) / 1 ether;
+
+        // Iterate to all assets
+        for(uint8 i; i < 16; ++i){
+            if(assetsIn & (uint16(1) << i) != 0){
+                if(borrowAmountToFulfill >= 0){
+                    return true;
+                }
+
+                address tokenAddress = indexToToken[i];
+                AssetInfo memory asset = tokenInfos[tokenAddress];
+                uint256 newAmount = collaterals[_user][tokenAddress] * getPrice(asset.priceFeed) / 1 ether;
+                borrowAmountToFulfill += int256(newAmount * asset.borrowCollateralFactor / 1 ether);
+            }
+        }
+
+        return borrowAmountToFulfill >= 0;
+    }
+
+    function repayAndSupplyAmount(int256 oldPrincipal, int256 newPrincipal) internal pure returns(uint256, uint256){
+        if(newPrincipal < oldPrincipal) return(0, 0);
+
+        if(newPrincipal <= 0){
+            return(uint256(newPrincipal - oldPrincipal), 0);
+        } else if(oldPrincipal >= 0){
+            return (0, uint256(newPrincipal - oldPrincipal));
+        } else {
+            return (uint256(-oldPrincipal), uint256(newPrincipal));
+        }
+    }
+
+    function withdrawAndBorrowAmount(int256 principal, int256 newPrincipal) internal pure returns(uint256, uint256){
+        if(newPrincipal > principal) return(0, 0);
+
+        if(newPrincipal >= 0){
+            return(uint256(principal - newPrincipal), 0);
+        } else if(principal <= 0){
+            return(0, uint256(principal - newPrincipal));
+        } else {
+            return(uint256(principal), uint256(-newPrincipal));
+        }
+    }
 
     function accrueInternal() internal {
         uint256 now_ = block.timestamp;
@@ -86,22 +236,22 @@ contract CompoundV3 {
         }
     }
 
-    function accruedInterestIndices(uint256 timeElapsed) internal {
+    function accruedInterestIndices(uint256 _timeElapsed) internal view returns(uint256, uint256){
         uint256 baseSupplyIndex = supplyInterestRate;
         uint256 baseBorrowIndex = borrowInterestRate;
-        if(timeElapsed > 0){
+        if(_timeElapsed > 0){
             uint256 utilization = getUtilization();
             uint256 supplyRate = getSupplyRate(utilization);
             uint256 borrowRate = getBorrowRate(utilization);
-            baseSupplyIndex += baseSupplyIndex * (supplyRate * timeElapsed) / 1 ether;
-            baseBorrowIndex += baseBorrowIndex * (borrowRate * timeElapsed) / 1 ether;
+            baseSupplyIndex += baseSupplyIndex * (supplyRate * _timeElapsed) / 1 ether;
+            baseBorrowIndex += baseBorrowIndex * (borrowRate * _timeElapsed) / 1 ether;
         }
         return (baseSupplyIndex, baseBorrowIndex);
     }
 
     function getUtilization() public view returns(uint256){
         uint256 totalSupply = presentValueSupply(supplyInterestRate, totalSupplyBase);
-        uint256 totalSBorrow = presentValueBorrow(borrowInterestRate, totalBorrowBase);
+        uint256 totalBorrow = presentValueBorrow(borrowInterestRate, totalBorrowBase);
         if(totalSupply == 0){
             return 0;
         } else {
@@ -109,33 +259,59 @@ contract CompoundV3 {
         }
     }
 
-    function presentValue(uint256 interestRate, uint256 principalValue) internal pure returns(uint256){
-        return principalValue * interestRate / 1 ether;
-    }
-
-    function presentValueSupply(uint256 interestRate, uint256 principalValue) internal pure returns(uint256){
-        return principalValue * interestRate / 1 ether;
-    }
-
-    function presentValueBorrow(uint256 interestRate, uint256 principalValue) internal pure returns(uint256){
-        return principalValue * interestRate / 1 ether;
-    }
-
-    function getSupplyRate(uint256 utilization) public view returns(uint256){
-        if(utilization <= optimalUtilizationSupply){
-            return supplyInterestBase + supplyInterestSlopeLow * utilization / 1 ether;
+    function presentValue(int256 _principalValue) internal view returns(int256){
+        if(_principalValue >= 0){
+            return int256(presentValueSupply(supplyInterestRate, uint256(_principalValue)));
         } else {
-            return supplyInterestBase + supplyInterestSlopeLow * optimalUtilizationSupply / 1 ether + supplyInterestSlopeHigh * (utilization - optimalUtilizationSupply) / 1 ether;
+            return -int256(presentValueBorrow(borrowInterestRate, uint256(-_principalValue)));
         }
     }
 
-    function getBorrowRate(uint256 utilization) public view returns(uint256){
-        if(utilization <= optimalUtilizationBorrow){
-            return borrowInterestBase + borrowInterestSlopeLow * utilization / 1 ether;
+    function principalValue(int256 _presentValue) internal view returns(int256){
+        if(_presentValue >= 0){
+            return int256(principalValueSupply(supplyInterestRate, uint256(_presentValue)));
         } else {
-            return borrowInterestBase + borrowInterestSlopeLow * optimalUtilizationBorrow / 1 ether + borrowInterestSlopeHigh * (utilization - optimalUtilizationBorrow) / 1 ether;
+            return -int256(principalValueBorrow(borrowInterestRate, uint256(-_presentValue)));
         }
     }
 
+    function principalValueSupply(uint256 _interestRate, uint256 _presentValue) internal pure returns(uint256){
+        return(_presentValue * 1 ether / _interestRate);
+    }
 
+    function principalValueBorrow(uint256 _interestRate, uint256 _presentValue) internal pure returns(uint256){
+        return((_presentValue * 1 ether + _interestRate - 1) / _interestRate);
+    }
+
+    function presentValueSupply(uint256 _interestRate, uint256 _principalValue) internal pure returns(uint256){
+        return _principalValue * _interestRate / 1 ether;
+    }
+
+    function presentValueBorrow(uint256 _interestRate, uint256 _principalValue) internal pure returns(uint256){
+        return _principalValue * _interestRate / 1 ether;
+    }
+
+    function getSupplyRate(uint256 _utilization) public view returns(uint256){
+        if(_utilization <= optimalUtilizationSupply){
+            return supplyInterestBase + supplyInterestSlopeLow * _utilization / 1 ether;
+        } else {
+            return supplyInterestBase + supplyInterestSlopeLow * optimalUtilizationSupply / 1 ether + supplyInterestSlopeHigh * (_utilization - optimalUtilizationSupply) / 1 ether;
+        }
+    }
+
+    function getBorrowRate(uint256 _utilization) public view returns(uint256){
+        if(_utilization <= optimalUtilizationBorrow){
+            return borrowInterestBase + borrowInterestSlopeLow * _utilization / 1 ether;
+        } else {
+            return borrowInterestBase + borrowInterestSlopeLow * optimalUtilizationBorrow / 1 ether + borrowInterestSlopeHigh * (_utilization - optimalUtilizationBorrow) / 1 ether;
+        }
+    }
+
+    // Notice it does not check for stale price
+    function getPrice(address _priceFeed) internal view returns(uint256){
+        (, int price, , , ) = IPriceFeed(_priceFeed).latestRoundData();
+        if (price <= 0) revert();
+        uint8 priceFeedDecimals = IPriceFeed(_priceFeed).decimals();
+        return uint256(price) * 10 ** (18 - priceFeedDecimals);
+    }
 }
